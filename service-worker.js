130adbb2da29e95b0b774002a22b07635354abfe
sudhirkots <sudhirkothari@gmail.com>
2026-05-25 10:36:09 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const cacheName = "weekly-dinner-generator-v1";
+const appFiles = [
+    "./",
+    "./index.html",
+    "./weekly_dinner_generator.html",
+    "./manifest.webmanifest",
+    "./app-icon.svg"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(appFiles))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+        ))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
